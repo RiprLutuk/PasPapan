0a677030d5ab86b2b7b16d9cc6b297f577a7f107
Ripr Lutuk <rizqy.pratama85@gmail.com>
2026-04-10 17:21:44 +0700

    fix: implement ShouldQueue on AssetReturnOtpRequested notification to prevent blocking request

--- app/Notifications/AssetReturnOtpRequested.php
+++ app/Notifications/AssetReturnOtpRequested.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 
-class AssetReturnOtpRequested extends Notification
+class AssetReturnOtpRequested extends Notification implements ShouldQueue
 {
     use Queueable;
 
