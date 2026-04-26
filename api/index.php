3acae6bb481c8a9e079bd304ee96d94fafbdb8c4
Ripr Lutuk <rizqy.pratama85@gmail.com>
2026-04-27 00:13:45 +0700

    fix: use writable vercel cache paths

--- api/index.php
+++ api/index.php
@@ -9,16 +9,30 @@ $storagePath = $_ENV['APP_STORAGE_PATH']
     ?? $_SERVER['APP_STORAGE_PATH']
     ?? getenv('APP_STORAGE_PATH')
     ?: '/tmp/storage';
+$cachePath = $storagePath.'/framework/cache';
 
 $_ENV['APP_STORAGE_PATH'] = $storagePath;
 $_SERVER['APP_STORAGE_PATH'] = $storagePath;
 putenv('APP_STORAGE_PATH='.$storagePath);
 
 foreach ([
+    'APP_CONFIG_CACHE' => $cachePath.'/config.php',
+    'APP_EVENTS_CACHE' => $cachePath.'/events.php',
+    'APP_PACKAGES_CACHE' => $cachePath.'/packages.php',
+    'APP_ROUTES_CACHE' => $cachePath.'/routes-v7.php',
+    'APP_SERVICES_CACHE' => $cachePath.'/services.php',
+] as $key => $value) {
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+    putenv($key.'='.$value);
+}
+
+foreach ([
     $storagePath.'/app',
     $storagePath.'/app/livewire-tmp',
     $storagePath.'/app/import-export/uploads',
     $storagePath.'/app/import-export/exports',
+    $cachePath,
     $storagePath.'/framework/cache/data',
     $storagePath.'/framework/sessions',
     $storagePath.'/framework/testing',
